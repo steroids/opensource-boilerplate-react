4b47516304d819895c388e268f72af7a03a9be3a
ProFIT <santiago.farias@yandex.ru>
2025-04-24 12:10:20 +0700

    steroids/dev#775 added boilerplate-ssr branch

--- webpack.js
+++ webpack.js
@@ -1,4 +1,20 @@
+const webpackNodeExternals = require('webpack-node-externals');
+
 require('@steroidsjs/webpack').config({
     inlineSvg: true,
     port: 9991,
+    ssr: {
+        externals: [
+            webpackNodeExternals({
+                allowlist: [/\.(?!(?:tsx?|jsx?|json)$).{1,5}$/i, /^lodash/, /^@steroidsjs/],
+            }),
+        ],
+        module: {
+            rules: {
+                ts: {
+                    exclude: /\.\/node_modules\/(?!steroidsjs\/core\/).*/,
+                },
+            },
+        },
+    },
 });
